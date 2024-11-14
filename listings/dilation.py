ea5e39c31259b3ad00f01db10ace5adda8b9c215
Arianna <arianna.soriani@studio.unibo.it>
2024-11-14 08:28:37 +0100

    updates

--- listings/dilation.py
+++ listings/dilation.py
@@ -1,10 +1,10 @@
-def dilatate_mask(image, dilation_size=5):
+def dilatate_mask(image, dilation_size=20):
 
     kernel = np.ones((dilation_size, dilation_size), np.uint8)
     dilatated_mask = cv2.dilate(image, kernel, iterations=1)
     return dilatated_mask
 
-def apply_mask_dilatated_crop(image_path, output_path, dilation_size=5):
+def apply_mask_dilatated_crop(image_path, output_path, dilation_size=20):
 
     image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
     mask_dilatated = dilatate_mask(image, dilation_size)
